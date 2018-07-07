41a1aa997424ec3ec8dead885528cb2f637b2715
Gerd Schmidt <gerd-has@awesome-co.de>
2018-07-07 21:21:26 +0200

    Add remaining time

---
+++ Assets/Flood/Scripts/VisualClock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualClock : MonoBehaviour {
+
+
+    public float RemainingTime { get; private set; }
+    public float MaxTime;
+
+    public GameObject ScaleGemObject;
+    private float _maxScaleGemObject;
+    private Vector3 _referencePosition;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _maxScaleGemObject = ScaleGemObject.transform.localScale.y;
+        _referencePosition = new Vector3(ScaleGemObject.transform.localPosition.x, ScaleGemObject.transform.localPosition.y, ScaleGemObject.transform.localPosition.z);
+
+        RemainingTime = MaxTime;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RemainingTime -= Time.deltaTime;
+
+        ScaleGemObject.transform.localScale = new Vector3(transform.localScale.x, (RemainingTime / MaxTime) * _maxScaleGemObject, transform.localScale.z);
+        ScaleGemObject.transform.localPosition = new Vector3(ScaleGemObject.transform.localPosition.x, _referencePosition.y - (_maxScaleGemObject - (((RemainingTime - 1) / MaxTime))), ScaleGemObject.transform.localPosition.z);
+
+
+        if (RemainingTime == 0)
+            Debug.Log("Time is up!");
+    }
+}
