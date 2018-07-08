a6afd5becb0f652ea7889100f99a374be1758bdc
Max Klock <maximilian.web@gmx.net>
2018-07-08 11:10:41 +0200

    adding level manager

---
+++ Assets/Flood/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject MRCameraParent;
+
+    public void LoadScene(string scene)
+    {
+        MRCameraParent.transform.position = Vector3.zero;
+        SceneManager.LoadScene(scene);
+    }
+
+    public void BackToMenu()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
